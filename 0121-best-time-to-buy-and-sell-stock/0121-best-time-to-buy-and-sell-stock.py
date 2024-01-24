0455a2c90c14d737132cb87cbe39bf1877853ec4
Sein Kim <119030948+Sein-0607@users.noreply.github.com>
2024-01-24 22:04:41 +0900

    Update 0121-best-time-to-buy-and-sell-stock.py

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,25 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:      
-        n = len(prices)
         profit = 0
-        min_price = max(prices) # 6
+        min_price = prices[0]
         
-        for curr in prices:
-            if curr < min_price:
-                min_price = curr
-                
-            elif curr - min_price > profit:
-                profit = curr - min_price
-            
+        for i in range(1, len(prices)):
+            profit = max(profit, prices[i]-min_price)
+            min_price = min(min_price, prices[i])
         return profit
+        
+
+        # 논리적이지 않은 풀이 
+        # n = len(prices)
+        # profit = 0
+        # min_price = max(prices) # 6
+        
+        # for curr in prices:
+        #     if curr < min_price:
+        #         min_price = curr
+                
+        #     elif curr - min_price > profit:
+        #         profit = curr - min_price
+            
+        # return profit
       
