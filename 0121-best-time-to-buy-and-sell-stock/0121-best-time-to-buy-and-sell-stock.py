80d9cc54456f5f2ec58b69dae0c3d1fae789a843
Sein Kim <119030948+Sein-0607@users.noreply.github.com>
2024-01-26 11:07:29 +0900

    Update 0121-best-time-to-buy-and-sell-stock.py

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,12 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:      
-        profit = 0
+        max_profit = 0
         min_price = prices[0]
+
+        for i in range(1, len(prices)): # day2부터 수익계산 시작
+            # i날의 수익
+            day_profit = prices[i] - min_price
+            if max_profit < day_profit:
+                max_profit = day_profit
+            # 새로운 조건 
+            if prices[i] < min_price:
+                min_price = prices[i]
+
+        return max_profit
         
-        for i in range(1, len(prices)):
-            profit = max(profit, prices[i]-min_price)
-            min_price = min(min_price, prices[i])
-        return profit
+
+
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:      
+#         profit = 0
+#         min_price = prices[0]
+        
+#         for i in range(1, len(prices)):
+#             profit = max(profit, prices[i]-min_price)
+#             min_price = min(min_price, prices[i])
+#         return profit
         
 
         # 논리적이지 않은 풀이 
